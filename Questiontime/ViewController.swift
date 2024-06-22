5ba177f91ebf468db1a6d07126793b788ec07ff7
Philip B. Osei <Paaseiedu@gmail.com>
2024-06-22 01:02:37 +0100

    Questiontime 0.1

--- Questiontime/ViewController.swift
+++ Questiontime/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewdidload run")
+        firstLabel.text  = "😃 Question time!"
+        
+        firstLabel.textColor = .red
+        secondLabel.textColor = . orange
     }
-
-
+    
+    @IBAction func firstButton(_ sender: UIButton) {
+        print("firstButton pressed")
+        firstLabel.text = "Larry Page"
+        secondLabel.text = "Sergey Brin"
+        firstLabel.textColor = .red
+        secondLabel.textColor = .orange    }
+    @IBAction func secondButton(_ sender: UIButton) {
+        print("secondButton pressed")
+        firstLabel.text = "Jenn Fleiss"
+        secondLabel.text = "Jenn hyman"
+        firstLabel.textColor = .red
+        secondLabel.textColor = .orange
+    }
+    
+    @IBAction func thirdButton(_ sender: UIButton) {
+        print("thirdButton pressed")
+        firstLabel.text = ""
+        secondLabel.text = ""
+        
+    }
 }
 
